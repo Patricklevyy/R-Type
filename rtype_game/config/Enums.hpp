bf2f37d37f84af9f11fce6a0e7f243c8c3ddb624
noethrone <noe.pereira.epitech.eu>
2024-11-26 10:25:27 +0100

    [ADD] config

---
+++ rtype_game/config/Enums.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Enums
+*/
+
+#ifndef ENUMS_HPP_
+    #define ENUMS_HPP_
+
+    namespace rtype
+    {
+        enum ROOM_ACTIONS {
+            UNDEFINED = -1,
+            CREATE,
+            JOIN,
+            LEAVE
+        };
+    }
+
+
+#endif /* !ENUMS_HPP_ */
