78473cd7288d4bbfd1b24b2b51ef97ff23756f1f
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2024-12-08 21:19:28 +0100

    [ADD] Hitbox component

---
+++ ecs/components/Hitbox.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Hitbox
+*/
+
+#ifndef HITBOX_HPP_
+#define HITBOX_HPP_
+
+namespace ecs {
+    class Hitbox {
+        public:
+
+            Hitbox() {};
+            ~Hitbox() {};
+
+            int width;
+            int height;
+        protected:
+        private:
+    };
+}
+
+#endif /* !HITBOX_HPP_ */
