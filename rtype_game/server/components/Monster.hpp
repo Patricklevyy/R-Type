3660deaccf1565796e7daf5b58326968619c34aa
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2024-12-16 20:26:41 +0100

    [ADD] Monster Component

---
+++ rtype_game/server/components/Monster.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Monster
+*/
+
+#ifndef MONSTER_HPP_
+#define MONSTER_HPP_
+
+namespace rtype {
+    class Monster {
+        public:
+            Monster() {};
+            ~Monster() {};
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !MONSTER_HPP_ */
