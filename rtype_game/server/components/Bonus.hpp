368a3a33f8db0f1696774045f860b69fc0357dbf
axel petitjean <axel.petitjean@epitech.eu>
2025-01-13 15:06:45 +0100

    [ADD] component bonus

---
+++ rtype_game/server/components/Bonus.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Bonus
+*/
+
+/**
+ * @file Bonus.hpp
+ * @brief Class representing bonus in the game.
+ *
+ * This class is used to define and manage ally bonus entities in the game.
+ */
+
+#ifndef BONUS_HPP_
+    #define BONUS_HPP_
+
+    namespace rtype
+    {
+        class Bonus {
+            public:
+                /**
+                 * @brief Constructor for the Bonus class.
+                 */
+                Bonus() {}
+
+                /**
+                 * @brief Destructor for the Bonus class.
+                 */
+                ~Bonus() {}
+
+            protected:
+            private:
+        };
+    }
+
+#endif /* !BONUS_HPP_ */
