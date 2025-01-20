4c6c280b37093e6798389775211b05717a419e62
axel petitjean <axel.petitjean@epitech.eu>
2025-01-20 21:56:45 +0100

    [UPD] put doxygen comments in shared

--- rtype_game/test_unitaire/connexion_tests.hpp
+++ rtype_game/test_unitaire/connexion_tests.hpp
@@ -11,6 +11,9 @@
     #include "../../ecs/udp/UDP_Client.hpp"
     #include "../../ecs/udp/UDP_Server.hpp"
 
+    /**
+     * @brief Test behavior when no configuration file is found.
+     */
     void test_no_config_file()
     {
         ecs::udp::UDP_Manager udp_manager;
@@ -18,18 +21,27 @@
         udp_manager.initialize("no_config_file.conf");
     }
 
+    /**
+     * @brief Test behavior when the configuration file is missing the secret key.
+     */
     void test_no_secret_key()
     {
         ecs::udp::UDP_Manager udp_manager;
         udp_manager.initialize("rtype_game/test_unitaire/files_tests/config_with_no_secret_key.config");
     }
 
+    /**
+     * @brief Test behavior when the buffer size in the configuration file is invalid.
+     */
     void test_wrong_buffer_size()
     {
         ecs::udp::UDP_Manager udp_manager;
         udp_manager.initialize("rtype_game/test_unitaire/files_tests/config_with_wrong_buffer_size.config");
     }
 
+    /**
+     * @brief Test behavior when the port specified in the configuration file is already in use.
+     */
     void test_port_already_used()
     {
         ecs::udp::UDP_Manager udp_manager;
@@ -37,12 +49,19 @@
         udp_manager.initialize("rtype_game/test_unitaire/files_tests/config_file.config");
     }
 
+
+    /**
+     * @brief Test behavior when the configuration file is missing the IP address.
+     */
     void test_no_ip()
     {
         ecs::udp::UDP_Manager udp_manager;
         udp_manager.initialize("rtype_game/test_unitaire/files_tests/config_with_no_ip.config");
     }
 
+    /**
+     * @brief Test behavior when attempting to send a message without initializing the UDP manager.
+     */
     void test_send_message_with_no_init()
     {
         ecs::udp::UDP_Manager udp_manager;
@@ -50,6 +69,9 @@
         udp_manager.sendMessage(std::vector<char>(), "");
     }
 
+    /**
+     * @brief Test behavior when attempting to send a message that exceeds the buffer size limit.
+     */
     void test_send_message_to_big()
     {
         ecs::udp::UDP_Manager udp_manager;
