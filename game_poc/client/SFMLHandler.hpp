aed89086e43088a6db96543824cb663733317d29
axel petitjean <axel.petitjean@epitech.eu>
2025-01-21 04:29:13 +0100

    [UPD] put doxygen comments in game_poc client

--- game_poc/client/SFMLHandler.hpp
+++ game_poc/client/SFMLHandler.hpp
@@ -14,14 +14,34 @@
     {
         class Client;
 
+        /**
+         * @class SFMLHandler
+         * @brief Handles events from SFML, such as mouse and keyboard interactions.
+         */
         class SFMLHandler {
             public:
+                /**
+                 * @brief Constructs the SFMLHandler object.
+                 * @param client The Client object which this handler will interact with.
+                 */
                 SFMLHandler(Client &client);
+
+                /**
+                 * @brief Destructor for SFMLHandler class.
+                 */
                 ~SFMLHandler();
 
+                /**
+                 * @brief Handles SFML events by processing each event in the queue.
+                 * @param events The queue of events to be processed.
+                 */
                 void handleEvents(std::queue<sf::Event> &);
 
             private:
+                /**
+                 * @brief Processes mouse button press events.
+                 * @param event The event triggered when a mouse button is pressed.
+                 */
                 void processMouseButtonPressed(const sf::Event &);
 
                 Client &_client;
